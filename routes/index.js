aebaa590b3b2df92c02ee35b63673de83c395bc8
Benjirez <benjirez@gmail.com>
2022-03-04 16:28:41 -0800

    monkey in the middle

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index')
-    console.log(`guess who's back...`)
-})
+router.get("/", (req, res) => {
+  res.render("index");
+  console.log(`guess who's back...`);
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-if (process.env.NODE_ENV !== 'production'){
-    //use .config() and not .load() or .parse()
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  //use .config() and not .load() or .parse()
+  require("dotenv").config();
 }
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-const indexRouter = require('./routes/index') 
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true } )
-const db = mongoose.connection
-db.on('error', error => { console.error(error) })
-db.once('open', ()=> { console.log('survey says... connected') })
+// Heroku deply, need to manually set config/env vars: Key, value to DATABASE_URL, "connection string from mongoDB/Compass including user and password"
 
+mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.error(error);
+});
+db.once("open", () => {
+  console.log("survey says... connected");
+});
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout','layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
 
-app.use('/', indexRouter)
+app.use("/", indexRouter);
 
-app.listen(process.env.PORT || 3000)
-
+app.listen(process.env.PORT || 3000);
